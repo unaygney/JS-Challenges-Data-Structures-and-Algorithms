b5bdde1a7bcdbb23cd9f6890dfafa1910f4a59cd
Guney Unay <guneymarketing17@hotmail.com>
2024-01-04 14:22:22 +0300

    challanges 09 is done

--- 01-basic-challenges-1/08-palindrome/palindrome-run.js
+++ 01-basic-challenges-1/08-palindrome/palindrome-run.js
@@ -1,6 +1,7 @@
-const isPalindrome = require('./palindrome');
+const isPalindrome = require("./palindrome");
 
-const result1 = isPalindrome('racecar');
-const result2 = isPalindrome('racecars');
+const result1 = isPalindrome("racecar");
+const result2 = isPalindrome("racecars");
+const result3 = isPalindrome("A man, a plan, a canal, Panama");
 
-console.log(result1, result2);
+console.log(result1, result2, result3);
