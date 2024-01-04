4f85673e4450102d17744b6ec9e7b86a75dd1f9c
Guney Unay <guneymarketing17@hotmail.com>
2024-01-04 14:51:46 +0300

    09 challanges is done

--- 01-basic-challenges-1/09-count-vowels/count-vowels-run.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels-run.js
@@ -1,5 +1,5 @@
-const countVowels = require('./count-vowels');
+const countVowels = require("./count-vowels");
 
-const result = countVowels('Hello World!');
+const result = countVowels("Hello World!");
 
 console.log(result);
