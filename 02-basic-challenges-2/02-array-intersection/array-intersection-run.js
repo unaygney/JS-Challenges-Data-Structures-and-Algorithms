0af8c2596a94ac419cf3e546234bfa741237c978
Guney Unay <guneymarketing17@hotmail.com>
2024-01-07 17:15:03 +0300

    array intersection challange is solved

--- 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
@@ -1,4 +1,4 @@
-const arrayIntersection = require('./array-intersection');
+const arrayIntersection = require("./array-intersection");
 
 const result = arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
 
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let newArray = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !newArray.includes(arr1[i])) {
+      newArray.push(arr1[i]);
+    }
+  }
+
+  return newArray;
+}
 
 module.exports = arrayIntersection;
