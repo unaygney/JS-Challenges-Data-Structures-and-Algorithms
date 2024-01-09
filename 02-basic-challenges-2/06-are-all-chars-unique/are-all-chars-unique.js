2cdefe4c963d8f51f00d8d0eb7f71ac14068885b
Guney Unay <guneymarketing17@hotmail.com>
2024-01-09 20:23:38 +0300

    are-all-chars-unique challanges is solved

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,19 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(string) {
+  if (!string) {
+    return true;
+  }
+
+  let uniqueArray = new Set();
+
+  for (let i = 0; i < string.length; i++) {
+    uniqueArray.add(string[i]);
+  }
+
+  if (string.length !== uniqueArray.size) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 module.exports = areAllCharactersUnique;
