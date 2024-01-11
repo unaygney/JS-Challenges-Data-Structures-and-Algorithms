dd0dfa91d4d2267842ecd6d1e39a1acd59127bde
Guney Unay <guneymarketing17@hotmail.com>
2024-01-11 23:28:02 +0300

    first non repeating test is done

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,14 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(string) {
+  let arr = {};
+  for (const char of string) {
+    arr[char] = (arr[char] || 0) + 1;
+  }
+  for (const char of string) {
+    if (arr[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
