84ab2812ad2c15a37393f4b18d8c0e475205a7ec
Guney Unay <guneymarketing17@hotmail.com>
2024-01-14 13:43:20 +0300

    format-phone-number challanges is solved ✅

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number-run.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number-run.js
@@ -1,4 +1,4 @@
-const formatPhoneNumber = require('./format-phone-number');
+const formatPhoneNumber = require("./format-phone-number");
 
 const result = formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 
--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,22 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(NumberArray) {
+  if (NumberArray && NumberArray.length === 10) {
+    let areaCode = addAreaCode(NumberArray);
+    let firstThreeNumber = [...NumberArray.slice(3, 6)].join("");
+    let restNumber = [...NumberArray.slice(6)].join("");
+
+    let formatNumber = `${areaCode} ${firstThreeNumber}-${restNumber}`;
+    return formatNumber;
+  } else {
+    throw new Error("Numbers must be 10 characters");
+  }
+}
+
+function addAreaCode(NumberArray) {
+  let areaCode;
+  areaCode = NumberArray.slice(0, 3);
+  areaCode.push(")");
+  areaCode.unshift("(");
+  return areaCode.join("");
+}
 
 module.exports = formatPhoneNumber;
