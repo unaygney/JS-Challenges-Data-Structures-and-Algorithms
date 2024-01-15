7eff5c160f41868cbcf2fd9856bf872e3017bc6f
Guney Unay <guneymarketing17@hotmail.com>
2024-01-15 16:11:31 +0300

    validate email challanges is solved !

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,9 @@
-function validateEmail() {}
+function validateEmail(string) {
+  const regex = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+
+  return regex.test(string);
+}
 
 module.exports = validateEmail;
