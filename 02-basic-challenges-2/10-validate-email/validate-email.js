d4984a75e4b936f69df47f42e3612f8099877d85
Guney Unay <guneymarketing17@hotmail.com>
2024-01-16 12:08:04 +0300

    validate email challange is solved by without regex

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,9 +1,27 @@
-function validateEmail(string) {
-  const regex = new RegExp(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
+// function validateEmail(string) {
+//   const regex = new RegExp(
+//     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//   );
 
-  return regex.test(string);
+//   return regex.test(string);
+// }
+
+//solution 2 => without regex
+
+function validateEmail(email) {
+  // Check if the email contains the "@" symbol
+  if (email.indexOf("@") === -1) {
+    return false;
+  }
+  const [localPart, domain] = email.split("@");
+  if (localPart.length === 0 || domain.length < 3) {
+    return false;
+  }
+  const domainExtension = domain.split(".");
+  if (domainExtension.length < 2 || domainExtension[1].length < 2) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = validateEmail;
