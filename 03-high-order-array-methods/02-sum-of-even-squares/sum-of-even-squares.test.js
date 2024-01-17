7e03a0a73549c235e201e0bd0fcd48f2d5893441
Guney Unay <guneymarketing17@hotmail.com>
2024-01-17 18:00:04 +0300

    test is done

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.test.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.test.js
@@ -1,6 +1,6 @@
-const sumOfEvenSquares = require('./sum-of-even-squares');
+const sumOfEvenSquares = require("./sum-of-even-squares");
 
-test('Sum of even squares', () => {
+test("Sum of even squares", () => {
   expect(sumOfEvenSquares([1, 2, 3, 4, 5])).toBe(20);
   expect(sumOfEvenSquares([-1, 0, 1, 2, 3, 4])).toBe(20);
   expect(sumOfEvenSquares([])).toBe(0);
