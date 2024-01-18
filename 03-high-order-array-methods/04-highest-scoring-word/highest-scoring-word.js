a70e3f8649da44b8fa31a8e5e094f8825a66a5ef
Guney Unay <guneymarketing17@hotmail.com>
2024-01-18 16:49:23 +0300

    highest-scoring-words challange is solved

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,13 @@
-function highestScoringWord() {}
+function highestScoringWord(string) {
+  let words = string.toLowerCase().split(" ");
+  let points = words.map((word, idx) => {
+    return word
+      .split("")
+      .reduce((acc, char) => acc + char.charCodeAt(0) - 96, 0);
+  });
+  let highestPoint = Math.max(...points);
+  let highestIndex = points.indexOf(highestPoint);
+  return words[highestIndex];
+}
 
 module.exports = highestScoringWord;
