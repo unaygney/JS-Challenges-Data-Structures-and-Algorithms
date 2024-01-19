dbee63b2b3d6db5d0891faeca5ffadc728b69e92
Guney Unay <guneymarketing17@hotmail.com>
2024-01-19 15:50:23 +0300

    valid anagrams challange is solved

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,9 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const string1 = str1.split("");
+  const string2 = str2.split("");
+
+  let isValid = string2.every((element) => string1.includes(element));
+  return isValid;
+}
 
 module.exports = validAnagrams;
