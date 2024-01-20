39548699d90abab189bd43f2d95236ae5598f1cb
Guney Unay <guneymarketing17@hotmail.com>
2024-01-20 18:40:28 +0300

    generate hashtag function challange is solved

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,17 @@
-function generateHashtag() {}
+function generateHashtag(string) {
+  if (!string || string.trim().length > 140) {
+    return false;
+  }
+  let capitalizeWord = string
+    .trim()
+    .replace(/\s+/g, " ")
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join("");
+
+  let newString = `#${capitalizeWord}`;
+
+  return newString;
+}
 
 module.exports = generateHashtag;
