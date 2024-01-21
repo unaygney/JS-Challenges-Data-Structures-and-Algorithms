f1863761ae7f54b9a1d54f8310d75689b553939c
Guney Unay <guneymarketing17@hotmail.com>
2024-01-21 13:16:38 +0300

    is-valid-ipv4 challange is solved ✅

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,19 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  let SPLIT_ARRAY = str.split(".");
+  let STRING_LENGTH = SPLIT_ARRAY.length;
+  // Check array length if greater than 4 return false
+  if (STRING_LENGTH !== 4) {
+    return false;
+  }
+
+  // does start with zero??
+  for (let i = 0; i < STRING_LENGTH; i++) {
+    if (SPLIT_ARRAY[i].startsWith("0") || SPLIT_ARRAY[i].endsWith("0")) {
+      return false;
+    }
+  }
+
+  return true;
+};
 
 module.exports = isValidIPv4;
