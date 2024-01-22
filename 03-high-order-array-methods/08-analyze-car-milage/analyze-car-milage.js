74606b36d63455ca72556f97f8b8b95d31fd0be0
Guney Unay <guneymarketing17@hotmail.com>
2024-01-22 12:54:02 +0300

    analyze-card-mileage challange is solved

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,17 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  let CARS_LENGTH = cars.length;
+  let totalMileage = cars.reduce((acc, cars) => acc + cars.mileage, 0);
+  let averageMileage = totalMileage / CARS_LENGTH;
+
+  // Mileage Value min and max
+  let highestMileage = Math.max(...cars.map((car) => car.mileage));
+  let lowestMileage = Math.min(...cars.map((car) => car.mileage));
+
+  // max and mix mileage car obj
+  let highestMileageCar = cars.find((car) => car.mileage === highestMileage);
+  let lowestMileageCar = cars.find((car) => car.mileage === lowestMileage);
+
+  return { averageMileage, highestMileageCar, lowestMileageCar, totalMileage };
+}
 
 module.exports = analyzeCarMileage;
