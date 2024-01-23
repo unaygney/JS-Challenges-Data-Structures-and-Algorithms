931f22dd31e426267223786e2fc6f0ce2f2dc6ff
Guney Unay <guneymarketing17@hotmail.com>
2024-01-23 11:25:50 +0300

    find-missing-lettet challange is solved

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,20 @@
-function findMissingLetter() {}
+function findMissingLetter(str) {
+  //string length
+  let STRING_LENGTH = str.length;
+
+  // string first letter value
+  let start = str[0].charCodeAt(0);
+
+  //expected array value
+  let expectedTotal = Array.from(
+    { length: STRING_LENGTH + 1 },
+    (_, index) => start + index
+  ).reduce((acc, curr) => acc + curr, start);
+
+  let total = str.reduce((acc, curr) => acc + curr.charCodeAt(0), start);
+  let missingLetter = String.fromCharCode(expectedTotal - total);
+
+  return missingLetter;
+}
 
 module.exports = findMissingLetter;
