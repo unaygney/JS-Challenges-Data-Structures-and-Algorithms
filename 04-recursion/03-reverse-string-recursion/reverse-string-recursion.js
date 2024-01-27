5a0a27da048fb773c4eb3078778d9a5899625fd0
Guney Unay <guneymarketing17@hotmail.com>
2024-01-27 16:13:32 +0300

    reverse-string-recursion is solved

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
+}
 
 module.exports = reverseString;
