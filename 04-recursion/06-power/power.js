7c68cd1d77216351a1ef9adb4dca5bb2ffce5531
Guney Unay <guneymarketing17@hotmail.com>
2024-01-28 12:40:22 +0300

    power challange solved

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,8 @@
-function power() {}
-
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
 module.exports = power;
