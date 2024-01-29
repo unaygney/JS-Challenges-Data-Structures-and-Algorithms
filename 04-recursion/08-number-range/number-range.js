4b5f44610873703423931e5ecf2bf383498e8352
Guney Unay <guneymarketing17@hotmail.com>
2024-01-29 17:18:15 +0300

    number range challange solved

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,12 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  } else {
+    let arr = [];
+    arr = numberRange(startNum, endNum - 1);
+    arr.push(endNum);
+    return arr;
+  }
+}
 
 module.exports = numberRange;
