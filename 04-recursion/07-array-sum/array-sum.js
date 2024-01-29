58cef898a2a13a6b1ea921f6bd336b477f4a3f99
Guney Unay <guneymarketing17@hotmail.com>
2024-01-29 17:17:31 +0300

    array sum challange is solved

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,11 @@
-function arraySum() {}
+function arraySum(number) {
+  let LENGTH = number.length;
+
+  if (LENGTH === 0) {
+    return 0;
+  } else {
+    return number[0] + arraySum(number.slice(1));
+  }
+}
 
 module.exports = arraySum;
