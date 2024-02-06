b49413fb59ef5a37f6180ea260ba5c440da8c176
Guney Unay <guneymarketing17@hotmail.com>
2024-02-06 16:45:19 +0300

    max-subbaray-sum solved

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,17 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = 0;
+
+  for (let i = 0; i <= arr.length - k; i++) {
+    let currentSum = 0;
+
+    for (let j = i; j < i + k; j++) {
+      currentSum += arr[j];
+    }
+
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
